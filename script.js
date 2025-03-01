44469d88c68cb680f9d067791025d57bf8adec4e
Prathmesh Nitnaware <166001818+prathmesh-nitnaware@users.noreply.github.com>
2025-03-01 10:20:41 +0530

    Adding all the file

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('gmailBtn').addEventListener('click', () => {
+    window.location.href = 'mailto:nitnaware.prathmesh@gmail.com';
+});
+
+document.getElementById('githubBtn').addEventListener('click', () => {
+    window.location.href = 'https://github.com/prathmesh-nitnaware';
+});
+
+document.getElementById('portfolioBtn').addEventListener('click', () => {
+    alert('Will be updated Soon!');
+});
+
+document.getElementById('linkedinBtn').addEventListener('click', () => {
+    window.location.href = 'https://www.linkedin.com/in/prathmesh-nitnaware-92570b292';
+});
+
+document.getElementById('showcaseBtn').addEventListener('click', () => {
+    window.location.href = 'certi.html';
+});
